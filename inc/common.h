ee7c87074942bfae25955e922a0970a8d21dab6a
serhatgenc74 <serhatgenc74@gmail.com>
2026-05-27 21:39:41 +0300

    First Init

---
+++ inc/common.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdint.h>
+#ifndef COMMON_H
+#define COMMON_H
+
+typedef uint8_t byte;
+typedef uint8_t word;
+
+#endif
---
+++ inc/cpu-internal.h
@@ -0,0 +1,42 @@
+#include "common.h"
+#ifndef INTERNAL_H
+#define INTERNAL_H
+
+typedef enum 
+{
+    FLAG_CARRY     = 0x01,
+    FLAG_ZERO      = 0x02,
+    FLAG_INTERRUPT = 0x04,
+    FLAG_DECIMAL   = 0x08,
+    FLAG_BREAK     = 0x10,
+    FLAG_UNUSED    = 0x20,
+    FLAG_OVERFLOW  = 0x40,
+    FLAG_NEGATIVE  = 0x80
+} Cpu_Flags_t;
+
+typedef enum 
+{
+    BP_CARRY     = 0,
+    BP_ZERO      = 1,
+    BP_INTERRUPT = 2,
+    BP_DECIMAL   = 3,
+    BP_BREAK     = 4,
+    BP_UNUSED    = 5,
+    BP_OVERFLOW  = 6,
+    BP_NEGATIVE  = 7
+} Cpu_Flags_BP_t;
+
+typedef struct 
+{
+word pc;                //Program Counter
+byte sp;                //Stack Pointer
+byte a,x,y ;            // Registers
+byte flag;              //Flags
+byte cpu_ram[0xFFFF];   //RAM
+
+} CPU;
+
+
+void (*OPCODES[256])();
+
+#endif
